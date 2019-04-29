0b8328a78631ad91b0b1a22ef2aa65731c1674ee
Joseph Harkleroad <hark130@yahoo.com>
2019-04-29 08:59:13 -0500

    master: shwarm_it.c initial commit
    Placeholder source file merely prints "Hellow shwarm!"

---
+++ shwarm_it.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  // puts()
+
+
+int main(void)
+{
+    // LOCAL VARIABLES
+    int retVal = 0;
+    
+    // SHWARM IT
+    // TO DO: DON'T DO NOW...
+    puts("Hello shwarm!");
+
+    // DONE
+    return retVal;
+}

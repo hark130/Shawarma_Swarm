4ed766d089438d17fe047634e111d39779027d7c
Joseph Harkleroad <hark130@yahoo.com>
2019-04-29 11:27:51 -0500

    p1: Updated Harkleswarm.c
    Added local allocation macro

--- Harkleswarm.c
+++ Harkleswarm.c
@@ -1,3 +1,8 @@
 #include "Harkleswarm.h"
 
+#ifndef HARKLESWARM_MAX_TRIES
+// MACRO to limit repeated allocation attempts
+#define HARKLESWARM_MAX_TRIES 3
+#endif  // HARKLESWARM_MAX_TRIES
+
 // PLACEHOLDER

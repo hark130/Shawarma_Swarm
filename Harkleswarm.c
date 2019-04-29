f06629e66d4634b9f976243637ec0138964af516
Joseph Harkleroad <hark130@yahoo.com>
2019-04-29 13:29:55 -0500

    p1: Defined some of Harkleswarm.c
    Defined:
        alloc_shawarma_struct()
        build_new_shawarma_struct()
    
    Written offline.  Untested.

--- Harkleswarm.c
+++ Harkleswarm.c
@@ -1,3 +1,4 @@
+#include "Harklecurse.h"
 #include "Harkleswarm.h"
 
 #ifndef HARKLESWARM_MAX_TRIES
@@ -5,4 +6,36 @@
 #define HARKLESWARM_MAX_TRIES 3
 #endif  // HARKLESWARM_MAX_TRIES
 
-// PLACEHOLDER
+
+shawarma_ptr alloc_shawarma_struct(void)
+{
+    return allocate_cartCoord_struct();
+}
+
+
+shawarma_ptr build_new_shawarma_struct(int xVal, int yVal, int shNum, char shChar, unsigned long shStatus)
+{
+    // LOCAL VARIABLES
+    shawarma_ptr retVal = NULL;
+    
+    // ALLOCATE & BUILD
+    retVal = build_new_cartCoord_struct(xVal, yVal, shChar, shStatus);
+    
+    // UPDATE
+    if (retVal)
+    {
+        retVal->posNum = shNum;
+    }
+    
+    // DONE
+    return retVal;
+}
+
+
+hcCartCoord_ptr add_shawarma_node(shawarma_ptr headShawarma, shawarma_ptr newShawarma, int newPos);
+
+
+bool free_shawarma_struct(shawarma_ptr* oldStruct_ptr);
+
+
+bool free_shawarma_linked_list(shawarma_ptr* oldHeadNode_ptr);

8b34a59c823cc6883dca249dcb109ae679a3731a
Joseph Harkleroad <hark130@yahoo.com>
2019-04-29 15:34:05 -0500

    p1: Defined create_shawarma_list in Harkleswarm.c
    Except for the rando_unique_coordinates() placeholder, create_shawarma_list() appears to be fully implemented in Harkleswarm.c.
    Written offline.  Untested.

--- Harkleswarm.c
+++ Harkleswarm.c
@@ -38,6 +38,130 @@ shawarma_ptr add_shawarma_node(shawarma_ptr headShawarma, shawarma_ptr newShawar
 }
 
 
+shawarma_ptr create_shawarma_list(int xMin, int xMax, int yMin, int yMax, int listLen, char shChar, unsigned long shStatus)
+{
+    // LOCAL VARIABLES
+    shawarma_ptr retVal = NULL;       // Store the head node to the new linked list here
+    shawarma_ptr tmp_ptr = NULL;      // Temp storage for newly built child nodes
+    shawarma_ptr tmpHead_ptr = NULL;  // Temp storage for newly built child nodes
+    bool success = true;              // Set this to false if anything fails
+    localShChar = shChar;             // Level of indirection for the struct graphic member
+    int i = 0;                        // Iterating variable
+    cartPnt newCoord = {0,0};         // Temp struct to use as 'out' parameter for rando_unique_coordinates() 
+    
+    // INPUT VALIDATION
+    if (xMin > xMax)
+    {
+        HARKLE_ERROR(Harkleswarm, create_shawarma_list, Invalid x coordinate min and max);
+        success = false;
+    }
+    else if (yMin > yMax)
+    {
+        HARKLE_ERROR(Harkleswarm, create_shawarma_list, Invalid y coordinate min and max);
+        success = false;
+    }
+    else if (listLen < 1)
+    {
+        HARKLE_ERROR(Harkleswarm, create_shawarma_list, Invalid list length);
+        success = false;
+    }
+    
+    // ALLOCATE NODES
+    if (true == success)
+    {
+        for (i = 1; i <= listLen; i++)
+        {
+            // Determine character value
+            if (0 == shChar)
+            {
+                localShChar = i;
+            }
+            
+            // First node
+            if (!retVal)
+            {
+                newCoord.xCoord = rando_me(xMin, xMax);
+                newCoord.yCoord = rando_me(yMin, yMax);
+                retVal = build_new_shawarma_struct(newCoord.xCoord, newCoord.yCoord, i, localShChar, shStatus);
+                if (!retVal)
+                {
+                    HARKLE_ERROR(Harkleswarm, create_shawarma_list, build_new_shawarma_struct failed);
+                    success = false;
+                }
+            }
+            // Additional node
+            else
+            {
+                // 1. Randomize some coordinates
+                // TO DO: DON'T DO NOW
+                // Use the struct cartesianCoordinate from Harklemath as an 'out' parameter for a new function
+                // that randomizes coordinates, walks the linked list, and verifies those coordinates are unique.
+                // if maxSearch is 0, searches forevert
+                // bool rando_unique_coordinates(shawarma_ptr headNode, cartPnt_ptr cartCoord_ptr, int maxSearch);
+                
+                // 2. Create a node
+                tmp_ptr = build_new_shawarma_struct(newCoord.xCoord, newCoord.yCoord, i, localShChar, shStatus);
+                if (!tmp_ptr)
+                {
+                    HARKLE_ERROR(Harkleswarm, create_shawarma_list, build_new_shawarma_struct failed);
+                    success = false;
+                }
+                else
+                {
+                    // 3. Add the node to the list
+                    tmpHead_ptr = add_shawarma_node(retVal, tmp_ptr, listLen + 1);
+                    
+                    if (tmpHead_ptr != retVal && retVal)
+                    {
+                        HARKLE_ERROR(Harkleswarm, create_shawarma_list, add_shawarma_node failed);
+                        success = false;
+                    }
+                }
+            }
+            
+            if (true == success)
+            {
+                // Reset temp variables because everything succeeded
+                localShChar = 0;
+                newCoord.xCoord = 0;
+                newCoord.yCoord = 0;
+                tmp_ptr = NULL;  // Not a memory leak because it's in the linked list
+                tmpHead_ptr = NULL;  // Not a memory leak because the head node pointer is also in retVal
+            }
+        }
+    }
+    
+    // CLEAN UP
+    if (false == success)
+    {
+        if (retVal)
+        {
+            // tmpHead_ptr
+            if (tmpHead_ptr == retVal)
+            {
+                tmpHead_ptr = NULL;
+            }
+            // retVal
+            if (false == free_shawarma_linked_list(&retVal))
+            {
+                HARKLE_ERROR(Harkleswarm, create_shawarma_list, free_shawarma_linked_list failed);
+            }
+            // tmp_ptr
+            if (tmp_ptr)
+            {
+                if (false == free_shawarma_struct(&tmp_ptr))
+                {
+                    HARKLE_ERROR(Harkleswarm, create_shawarma_list, free_shawarma_struct failed);
+                }
+            }
+        }
+    }
+    
+    // DONE
+    return retVal;
+}
+
+
 bool free_shawarma_struct(shawarma_ptr* oldStruct_ptr)
 {
     return free_cartCoord_struct(oldStruct_ptr);

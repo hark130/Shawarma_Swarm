f01407156e1dad3bde4d4ab6877a2e2a02a8bb55
hark130 <hark130@yahoo.com>
2019-05-03 01:26:30 -0500

    p1: Massive Harkleswarm.c Update
    count_entries()
    	- Added (and commented) DEBUGGING statements
    	- Fixed BUG
    	- Verified(?) functional
    replace_hsLineLen_array_entry()
    	- Defined
    find_longest_entry()
    	- Defined
    shwarm_one_dim()
    	- Updated for new intercepts prototype
    	- Defined much of the functionality
    	- Stubbed/commented remaining unfinished functionality
    find_closest_points()
    	- Updated for new prototype
    	- Defined all(?) functionality
    	- Not yet sure if functional
    	- Wait until shwarm_one_dim() is completely implemented... then we'll know
    shwarm_it()
    	- Updated for new intercepts parameter
    calc_hsLineLen_contents()
    	- Defined
    	- Not entirely sure if functional (but how could it now be if my distance function is working)
    
    GOOD NEWS: I'm not getting compiler errors or crashes or (unexpected) HARKLE_ERROR() messages.
    BAD NEWS: I'm not sure if most of this is working as intended.  We'll know more when we finish shwarm_one_dim().
    
    Changes to be committed:
    	modified:   Harkleswarm.c

--- Harkleswarm.c
+++ Harkleswarm.c
@@ -85,20 +85,150 @@ int count_entries(void* some_arr, size_t ptrSize)
             foundNull = true;  // Prove this wrong
             for (i = 0; i < ptrSize; i++)
             {
+                // printf("%d", (int)(*(tmp_ptr + i)));  // DEBUGGING
                 if (0x0 != (*(tmp_ptr + i)))
                 {
                     foundNull = false;
                 }
             }
+            // printf("\n");  // DEBUGGING
             if (false == foundNull)
             {
                 numEntries++;
+                tmp_ptr += ptrSize;
             }
         }
     }
     
     // DONE
     return numEntries;
+}
+
+
+/*
+    PURPOSE - Replace the oldDist entry in coord_arr with the data from newEntry_ptr
+    INPUT
+        oldEntry_ptr - The old entry in coord_arr to replace
+        newEntry_ptr - New data to replace the old data (see: oldEntry_ptr) with
+        coord_arr - NULL-terminated array of hsLineLen struct pointers to search through
+    OUTPUT
+        On success, true
+        On failure, false
+    NOTES
+        The newEntry_ptr will not overwrite any existing pointers in the coord_arr.  Rather, the data
+            from newEntry_ptr will overwrite the data in array entry that matches oldEntry_ptr.
+        Only the xCoord and yCoord member will be used to match oldEntry_ptr in coord_arr since
+            double comparisons can be imprecise.
+        This is a highly specialized function and, as flexible as it is, was never intended to be
+            used outside of Harkleswarm.  Maybe, one day...
+ */
+bool replace_hsLineLen_array_entry(hsLineLen_ptr oldEntry_ptr, hsLineLen_ptr newEntry_ptr, hsLineLen_ptr* coord_arr)
+{
+    // LOCAL VARIABLES
+    bool success = false;                // Set this to true if the entry is found and replaced
+    hsLineLen_ptr* tmp_arr = coord_arr;  // Iterating variable
+
+    // INPUT VALIDATION
+    if (!oldEntry_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, replace_entry, Invalid oldEntry_ptr);
+    }
+    else if (!newEntry_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, replace_entry, Invalid newEntry_ptr);
+    }
+    else if (!coord_arr)
+    {
+        HARKLE_ERROR(Harkleswarm, replace_entry, Invalid coord_arr);
+    }
+    else if (NULL == *coord_arr)
+    {
+        HARKLE_ERROR(Harkleswarm, replace_entry, Empty coord_arr);
+    }
+    else
+    {
+        // FIND AND REPLACE
+        while (*tmp_arr && false == success)
+        {
+            // 1. Find
+            if ((*tmp_arr)->xCoord == oldEntry_ptr->xCoord && (*tmp_arr)->yCoord == oldEntry_ptr->yCoord)
+            {
+                // 2. Replace
+                (*tmp_arr)->xCoord = newEntry_ptr->xCoord;
+                (*tmp_arr)->yCoord = newEntry_ptr->yCoord;
+                (*tmp_arr)->dist = newEntry_ptr->dist;
+                success = true;
+                break;
+            }
+
+            // Next entry
+            tmp_arr++;
+        }
+    }
+
+    // DONE
+    return success;
+}
+
+
+/*
+    PURPOSE - Find the longest entry in coord_arr and copy the data into outNode_ptr
+    INPUT
+        coord_arr - NULL-terminated array of hsLineLen struct pointers to search through
+        outNode_ptr - The new storage location of the longest length hsLineLen struct
+        maxPrec - Maximum floating point precision when checking "dist" value against "dist" value
+    OUTPUT
+        On success, true
+        On failure, false
+    NOTES
+        Maximum precision will be used to compare "dist" member values
+        This is a highly specialized function and, as flexible as it is, was never intended to be
+            used outside of Harkleswarm.  Maybe, one day...
+ */
+bool find_longest_entry(hsLineLen_ptr* coord_arr, hsLineLen_ptr outNode_ptr, int maxPrec)
+{
+    // LOCAL VARIABLES
+    bool success = false;                // Set this to true when outNode_ptr is written to
+    hsLineLen_ptr* tmp_arr = coord_arr;  // Iterating variable
+
+    // INPUT VALIDATION
+    if (!coord_arr)
+    {
+        HARKLE_ERROR(Harkleswarm, find_longest_entry, Invalid coord_arr);
+    }
+    else if (NULL == *coord_arr)
+    {
+        HARKLE_ERROR(Harkleswarm, find_longest_entry, Empty coord_arr);
+    }
+    else if (!outNode_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, find_longest_entry, Invalid newEntry_ptr);
+    }
+    else
+    {
+        // FIND LONGEST DISTANCE ENTRY
+        success = true;  // We're good now
+        // Start with the first
+        outNode_ptr->xCoord = (*tmp_arr)->xCoord;
+        outNode_ptr->yCoord = (*tmp_arr)->yCoord;
+        outNode_ptr->dist = (*tmp_arr)->dist;
+        tmp_arr++;  // Next
+
+        while (*tmp_arr)
+        {
+            if (true == dble_greater_than((*tmp_arr)->dist, outNode_ptr->dist, maxPrec))
+            {
+                outNode_ptr->xCoord = (*tmp_arr)->xCoord;
+                outNode_ptr->yCoord = (*tmp_arr)->yCoord;
+                outNode_ptr->dist = (*tmp_arr)->dist;
+            }
+
+            tmp_arr++;
+        }
+    }
+
+    // DONE
+    return success;
 }
 
 
@@ -110,6 +240,7 @@ int count_entries(void* some_arr, size_t ptrSize)
         headNode_ptr - Pointer to the head node of a linked list of shawarma pointers containing available points
         sourceNode_ptr - shawarma struct pointer to use as the 'origin' point to calculate distances
         maxMoves - Number of one-dimensional moves, regardless of dimension, the node may move to pursue equilibrium
+        intercepts - If true, dimensional intercepts will be treated as points for the purposes of equilibrium
     OUTPUT
         On success, number of moves made (not to exceed maxMoves).  0 indicates success (and also equilibrium).
         On failure, -1
@@ -117,12 +248,16 @@ int count_entries(void* some_arr, size_t ptrSize)
         This function does not perform input validation.  It assumes the calling function has already validated
             the parameter being passed in.
  */
-int shwarm_one_dim(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int maxMoves, shawarma_ptr sourceNode_ptr)
+int shwarm_one_dim(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, shawarma_ptr sourceNode_ptr, int maxMoves, bool intercepts)
 {
     // LOCAL VARIABLES
-    int numMoves = -1;    // Number of moves made
-    bool success = true;  // Prove this wrong
-    double slope = 0.0;   // Store slope here
+    int numMoves = -1;     // Number of moves made
+    bool success = true;   // Prove this wrong
+    double slope = 0.0;    // Store slope here
+    int numClosePnts = 0;  // Number of points that find_closest_points() found
+    hsLineLen point1 = { 0, 0, 0.0 };  // Out parameter for find_closest_points()
+    hsLineLen point2 = { 0, 0, 0.0 };  // Out parameter for find_closest_points()
+    hsLineLen_ptr coord_arr[] = { &point1, &point2, NULL };
 
     // SWARM
     // 1. Verify minimum number of points (need two to get a slope)
@@ -146,19 +281,54 @@ int shwarm_one_dim(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int maxM
     if (true == success)
     {
         success = verify_line(headNode_ptr, slope, HS_MAX_SLOPE_PRECISION);
+
+        if (false == success)
+        {
+            HARKLE_ERROR(Harkleswarm, shwarm_one_dim, Provided points are not in a line);
+        }
     }
 
-    // 4. Find closest points
-    if (true == success)
+    // 4. Consider intercepts
+    if (true == success && true == intercepts)
     {
-        
+        // TO DO: DON'T DO NOW
+        // Add this as a p1-1 feature
+        // If I had to do it now:
+        //      A. write a line_intercept() function in Harklemath.*
+        //      B. Use that function to populate new shawarma nodes added to the end of the linked list
+        //      C. Let find_closest_point() do what it does
+        //      D. At the end of shwarm_one_dim(), remove & free those temporary "intercept" shawarma nodes from the linked list
     }
 
-    // 5. Consider intercepts
+    // 5. Find closest points
     if (true == success)
     {
-        
+        numClosePnts = find_closest_points(curWindow, headNode_ptr, sourceNode_ptr, coord_arr);
+
+        // One dimension should find two points (dim + 1)
+        if (-1 == numClosePnts)
+        {
+            HARKLE_ERROR(Harkleswarm, shwarm_one_dim, find_closest_points encountered an error);
+            success = false;
+        }
+        else if (0 == numClosePnts)
+        {
+            HARKLE_ERROR(Harkleswarm, shwarm_one_dim, find_closest_points failed);
+            success = false;
+        }
+        else if (2 != numClosePnts)
+        {
+            HARKLE_ERROR(Harkleswarm, shwarm_one_dim, find_closest_points found an invalid number of points);
+            success = false;
+        }
     }
+
+    // 6. Calculate center
+    // A. Determine slope, B. Determine distance, C. Find "distance / 2" along the slope from one of the closest points
+
+    // 7. Move the point closer
+    // TO DO: DON'T DO NOW... write a helper function that takes the sourceNode_ptr, the destination coordinate, and maxMoves
+    // Then that helper function moves the coordinates of sourceNode_ptr "maxMoves" moves towards the destination coordinate.
 
     // DONE
     return numMoves;
@@ -451,24 +621,40 @@ bool verify_unique_coordinates(int xCoord, int yCoord, shawarma_ptr headNode_ptr
 }
 
 
-int find_closest_points(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int srcNum, hsLineLen_ptr* coord_arr)
+int find_closest_points(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, shawarma_ptr sourceNode_ptr, hsLineLen_ptr* coord_arr)
 {
     // LOCAL VARIABLES
-    int numPoints = -1;               // -1 on error, number of points calculated on success
-    shawarma_ptr srcShawarma = NULL;  // Source point from which to base all calculations
+    int numPoints = -1;                // -1 on error, number of points calculated on success
+    bool success = true;               // Set this to false if anything fails
+    int i = 0;                         // Iterating variable for loops
+    shawarma_ptr tmpNode_ptr = NULL;   // Iterating variable for the 'in' linked list
+    hsLineLen_ptr *tmpArr_ptr = NULL;  // Iterating variable for the 'out' array
+    int tmpNum = 0;                    // Temporary number variable
+    // Use this as a temp 'out' parameter for calc_hsLineLen_contents()
+    hsLineLen localLineLen = { 0, 0, 0.0 };
+    // Use this to store the current value of the furthest point in coord_arr
+    hsLineLen biggestLineLen = { 0, 0, 0.0 };
     
     // INPUT VALIDATION
     if (!curWindow)
     {
         HARKLE_ERROR(Harkleswarm, find_closest_points, Invalid curWindow);
+        success = false;
     }
     else if (!headNode_ptr)
     {
         HARKLE_ERROR(Harkleswarm, find_closest_points, Invalid headNode_ptr);
+        success = false;
+    }
+    else if (!sourceNode_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, find_closest_points, Invalid sourceNode_ptr);
+        success = false;
     }
     else if (!coord_arr || NULL == *coord_arr)
     {
         HARKLE_ERROR(Harkleswarm, find_closest_points, Invalid coord_arr);
+        success = false;
     }
     else
     {
@@ -476,17 +662,99 @@ int find_closest_points(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int
     }
     
     // FIND POINTS
-    if (-1 < numPoints)
+    if (-1 < numPoints && true == success)
     {
-        // 1. Find the pointer to the source coordinate
-        srcShawarma = get_pos_num(headNode_ptr, srcNum);
-
-        // 2. Determine intended dimensions
+        // 1. Determine intended dimensions
         numPoints = count_entries(coord_arr, sizeof(hsLineLen_ptr));
         // printf("Found %d entries!", numPoints);  // DEBUGGING
 
-        // 3. Find nearest points given dimensions
-        // TO DO: DON'T DO NOW
+        // 2. Verify minimum number of nodes exist
+        if (numPoints > (get_num_cartCoord_nodes(headNode_ptr) - 1))
+        {
+            HARKLE_ERROR(Harkleswarm, find_closest_points, Not enough coord_arr entries);
+            numPoints = -1;
+        }
+        else
+        {
+            // 3. Find nearest points given dimensions
+            tmpNum = 0;
+            tmpNode_ptr = headNode_ptr;
+            tmpArr_ptr = coord_arr;
+
+            // A. Fill the array with the first "numPoints" number of node entries
+            tmpNum = 0;  // Reset temp variable
+            while (tmpNode_ptr && tmpNum < numPoints && true == success)
+            {
+                if (tmpNode_ptr != sourceNode_ptr)
+                {
+                    success = calc_hsLineLen_contents(sourceNode_ptr, tmpNode_ptr, *tmpArr_ptr);
+
+                    if (false == success)
+                    {
+                        HARKLE_ERROR(Harkleswarm, find_closest_points, calc_hsLineLen_contents failed);
+                        numPoints = 0;
+                    }
+                    else
+                    {
+                        tmpNum++;
+                        // New biggest distance?
+                        if (true == dble_greater_than((*tmpArr_ptr)->dist, biggestLineLen.dist, DBL_PRECISION))
+                        {
+                            biggestLineLen.xCoord = (*tmpArr_ptr)->xCoord;
+                            biggestLineLen.yCoord = (*tmpArr_ptr)->yCoord;
+                            biggestLineLen.dist = (*tmpArr_ptr)->dist;
+                        }
+                        tmpArr_ptr++;  // NOW increment
+                    }
+                }
+
+                tmpNode_ptr = tmpNode_ptr->nextPnt;
+            }
+            tmpArr_ptr = NULL;  // Don't use this anymore
+
+            // B. Check the rest of the linked list for closer entries
+            while (tmpNode_ptr && true == success)
+            {
+                if (tmpNode_ptr != sourceNode_ptr)
+                {
+                    // Calculate length
+                    success = calc_hsLineLen_contents(sourceNode_ptr, tmpNode_ptr, &localLineLen);
+
+                    if (false == success)
+                    {
+                        HARKLE_ERROR(Harkleswarm, find_closest_points, calc_hsLineLen_contents failed);
+                        numPoints = 0;
+                    }
+                    else
+                    {
+                        if (true == dble_less_than(localLineLen.dist, biggestLineLen.dist, DBL_PRECISION))
+                        {
+                            // Replace entry in coord_arr with localLineLen data
+                            success = replace_hsLineLen_array_entry(&biggestLineLen, &localLineLen, coord_arr);
+
+                            if (false == success)
+                            {
+                                HARKLE_ERROR(Harkleswarm, find_closest_points, replace_hsLineLen_array_entry failed);
+                                numPoints = 0;
+                            }
+                            else
+                            {
+                                // Replace "biggest" line length local variable with the "new biggest" line length
+                                success = find_longest_entry(coord_arr, &biggestLineLen, DBL_PRECISION);
+
+                                if (false == success)
+                                {
+                                    HARKLE_ERROR(Harkleswarm, find_closest_points, find_longest_entry failed);
+                                    numPoints = 0;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                tmpNode_ptr = tmpNode_ptr->nextPnt;
+            }
+        }
     }
     
     // DONE
@@ -494,7 +762,7 @@ int find_closest_points(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int
 }
 
 
-int shwarm_it(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int maxMoves, int srcNum, int numDim)
+int shwarm_it(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int maxMoves, int srcNum, int numDim, bool intercepts)
 {
     // LOCAL VARIABLES
     int numMoves = -1;                   // Store the return value from helper functions here
@@ -537,7 +805,7 @@ int shwarm_it(winDetails_ptr curWindow, shawarma_ptr headNode_ptr, int maxMoves,
             switch (numDim)
             {
                 case 1:
-                    numMoves = shwarm_one_dim(curWindow, headNode_ptr, maxMoves, sourceNode_ptr);
+                    numMoves = shwarm_one_dim(curWindow, headNode_ptr, sourceNode_ptr, maxMoves, intercepts);
                     break;
                 default:
                     HARKLE_ERROR(Harkleswarm, shwarm_it, Unsupported dimension);
@@ -595,6 +863,52 @@ bool verify_line(shawarma_ptr headNode_ptr, double slope, int maxPrec)
 
     // DONE
     return straightLine;
+}
+
+
+bool calc_hsLineLen_contents(shawarma_ptr sourceNode_ptr, shawarma_ptr destNode_ptr, hsLineLen_ptr outParam_ptr)
+{
+    // LOCAL VARIABLES
+    bool success = false;  // Prove this wrong
+
+    // INPUT VALIDATION
+    if (!sourceNode_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, calc_hsLineLen_contents, Invalid sourceNode_ptr);
+    }
+    else if (!destNode_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, calc_hsLineLen_contents, Invalid destNode_ptr);
+    }
+    else if (!outParam_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, calc_hsLineLen_contents, Invalid outParam_ptr);
+    }
+    else if (sourceNode_ptr == destNode_ptr)
+    {
+        HARKLE_ERROR(Harkleswarm, calc_hsLineLen_contents, Source and destination nodes may not be the same);
+    }
+    else if (sourceNode_ptr->absX == destNode_ptr->absX && sourceNode_ptr->absY == destNode_ptr->absY)
+    {
+        HARKLE_ERROR(Harkleswarm, calc_hsLineLen_contents, Source and destination coordinates may not be the same);
+    }
+    else
+    {
+        // POPULATE STRUCT
+        // 1. Get distance
+        outParam_ptr->dist = calc_int_point_dist(sourceNode_ptr->absX, sourceNode_ptr->absY,
+                                                 destNode_ptr->absX, destNode_ptr->absY);
+
+        // 2. Assign struct member values
+        outParam_ptr->xCoord = destNode_ptr->absX;
+        outParam_ptr->yCoord = destNode_ptr->absY;
+
+        // 3. Success!
+        success = true;
+    }
+
+    // DONE
+    return success;
 }
 
 
